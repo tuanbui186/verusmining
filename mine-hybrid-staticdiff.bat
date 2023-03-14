5d2ea9c0015f29d68f4ef17c84d878d6525dc4ae
tuanbui186 <11520451@gm.uit.edu.vn>
2023-03-14 08:18:04 +0700

    Add files via upload

---
+++ mine-hybrid-staticdiff.bat
@@ -0,0 +1,1 @@
+hellminer.exe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RDWx2Sa8iyeLVTMUUi5ypY357ZXH5tNf56.hellminer -p hybrid,d=16384s --cpu 2
---
+++ mine-hybrid.bat
@@ -0,0 +1,1 @@
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RR2irzCFVkNaZGsopir3YGkrPMWRBaPaN9.manhtuancomputer -p x --cpu 1
